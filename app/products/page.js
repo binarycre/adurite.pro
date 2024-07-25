146690d6b5a37c1efd62deed829516f937a8543a
Birdier <112028548+Birdier@users.noreply.github.com>
2024-07-25 17:07:30 +0200

    Update page.js
    Added products, changed prices and links.

--- app/products/page.js
+++ app/products/page.js
@@ -3,17 +3,35 @@ import React from 'react';
 export default function Home() {
   const products = [
     {
-      name: "Roblox Wild West | Auction Weapons & Items",
-      price: "$3.99 - $24.99",
+      name: "Roblox Wild West | Weapons & Items | READ DESCRIPTION!",
+      price: "$8.29 - $11.99",
       img: "https://i.ebayimg.com/images/g/BjsAAOSwt6NmSeyu/s-l960.webp",
-      link: "https://www.ebay.com/itm/145796452288"
+      link: "https://www.ebay.com/itm/145877219893"
     },
     {
       name: "Roblox Dragon Adventures | Solarizon",
-      price: "$7.69",
+      price: "$4.89",
       img: "https://i.ebayimg.com/images/g/lisAAOSwtgtlPlx2/s-l1600.webp",
       link: "https://www.ebay.com/itm/145399823766"
-    }
+    },
+    {
+      name: "Roblox Murder Mystery 2 (MM2) Weapons & Pets",
+      price: "$0.99 - $6.89",
+      img: "https://i.ebayimg.com/images/g/qdAAAOSw9phmkqkY/s-l1600.webp",
+      link: "https://www.ebay.com/itm/145907239411"
+    },
+    {
+      name: "Roblox Creatures of Sonaria (COS) | Creatures",
+      price: "$0.99 - $5.99",
+      img: "https://i.ebayimg.com/images/g/2yoAAOSw40hmoRsj/s-l1600.webp",
+      link: "https://www.ebay.com/itm/145908991806"
+    },
+    {
+      name: "Roblox Islands | Items & Weapons | Read Description !",
+      price: "$7.99 - $34.99",
+      img: "https://i.ebayimg.com/images/g/ZwAAAOSwEFZmoXAB/s-l1600.webp",
+      link: "https://www.ebay.com/itm/145909331008"
+    },
   ];
 
   return (
